374fa19493ef637029fdc6f14fa1d880a2fd3abb
Rishi Kumar <rishi.kumar@varahaworld.com>
2024-07-09 16:41:39 +0530

    redux configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
-import Config from "./config/config";
-
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
-  console.log(Config.appwriteUrl);
-  return <div className="bg-gray-700 text-white">Hello</div>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      } catch (error) {
+        console.error("Failed to fetch user data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, [dispatch]);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loding</div>
+  );
 }
 
 export default App;
